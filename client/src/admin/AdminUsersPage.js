4cfa1c94a7e6e8e786c4135a65d046339fdab13f
tijana2000n <tijana2000n@gmail.com>
2022-05-31 23:58:18 +0200

    adding users list in admin page
     Author:    tijana2000n <tijana2000n@gmail.com>

--- client/src/admin/AdminUsersPage.js
+++ client/src/admin/AdminUsersPage.js
@@ -1,11 +1,32 @@
-import React from 'react'
+import axios from "axios";
+import React, { useEffect } from "react";
+import { Container, Header } from "rsuite";
 
 function AdminUsersPage() {
-    return (
-        <div>
-            admin users list
-        </div>
-    )
+  const [users, setUsers] = React.useState([]);
+
+  const getUsers = async (u) => {
+    const res = await axios.get("/user", u);
+    setUsers(res.data);
+  };
+
+  useEffect(() => {
+    getUsers();
+  }, []);
+
+  console.log(users?.content);
+
+  return (
+    <Container>
+      <Header>Admin users list</Header>
+
+      <div>
+        {users?.content?.map((user) => (
+          <div>{user?.firstName}</div>
+        ))}
+      </div>
+    </Container>
+  );
 }
 
-export default AdminUsersPage
+export default AdminUsersPage;
--- server/src/route/userRouter.ts
+++ server/src/route/userRouter.ts
@@ -1,7 +1,29 @@
 import { Router } from "express";
+import { appDataSource } from "../dataSource";
+import { User } from "../entity/User";
+
+
 
 const router = Router();
 
+router.get('/', async (req, res) => {
+  const search = req.query.search as string | undefined;
+  const page = Number(req.query.page) || 0;
+  const size = Number(req.query.size) || 20;
+  const builder = appDataSource.getRepository(User).createQueryBuilder('user')
+  if (search && search.trim().length > 0) {
+    builder.where(`user.email LIKE :search`)
+      .orWhere(`CONCAT(user.firstName,' ', user.lastName) LIKE :search`)
+      .setParameter('search', '%' + search + '%');
+  }
+  const [data, count] = await builder.limit(page).offset(page * size).getManyAndCount()
+  res.json({
+    content: data,
+    totalElements: count,
+    size: data.length,
+    page: page
+  })
+})
 
 router.post('/logout', async (req, res) => {
   (req.session as any).user = undefined;
