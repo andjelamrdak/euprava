d5faafd1a44c5c02952d6bf3098d836d41daf318
anja.mrdak2 <anja.mrdak2@gmail.com>
2022-06-04 23:53:52 +0200

    adding endpoints for creating vacccines and criminal records

--- server/src/route/criminalProceeding.ts
+++ server/src/route/criminalProceeding.ts
@@ -1,5 +1,43 @@
+
+
 import { Router } from "express";
+import { appDataSource } from "../dataSource";
+import { CriminalProceeding } from "../entity/CriminalProceeding";
+import { User } from "../entity/User";
 
 const router = Router();
+
+router.get("/", async (_req, res) => {
+  res.json(
+    await appDataSource.getRepository(CriminalProceeding).find({
+      relations: {
+        user: true,
+      },
+      order: {
+        id: "DESC",
+      },
+    })
+  );
+});
+
+router.post("/", async (req, res) => {
+  const user = (req.session as any).user as User;
+
+  const post = await appDataSource.getRepository(CriminalProceeding).save({
+    ...req.body,
+    user,
+  });
+  res.json(post);
+});
+
+
+router.delete('/:id', async (req, res) => {
+    try {
+      await appDataSource.getRepository(CriminalProceeding).delete(req.params.id)
+      res.sendStatus(204);
+    } catch (error) {
+      res.status(500).json({ error: 'Error' })
+    }
+  })
 
 export default router;
